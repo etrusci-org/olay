c2e2afa81b3fe169575be63fed886cbbb50e1d76
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-11-05 13:31:10 +0100

    Nifty index page

---
+++ app/index.php
@@ -0,0 +1,34 @@
+<?php
+require __DIR__.'/lib/conf.php';
+
+include __DIR__.'/tpl/header.php';
+?>
+
+
+
+
+<h1>olay</h1>
+
+<h2>modules</h2>
+
+<ul>
+    <?php
+    foreach (array_keys(CONF['module']) as $k) {
+        printf('
+            <li>
+                <a href="overlay.php?module=%1$s">%1$s</a><br>
+                conf: <code>%2$s</code>
+            </li>',
+            $k,
+            json_encode(CONF['module'][$k]),
+        );
+    }
+    ?>
+</ul>
+
+
+
+
+
+
+<?php  include __DIR__.'/tpl/footer.php'; ?>
