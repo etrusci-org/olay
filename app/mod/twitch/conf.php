89bac6a0f1ba8db51acefa7f499af8b4c5e716db
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-01-25 14:53:22 +0100

    Janitor

--- app/mod/twitch/conf.php
+++ app/mod/twitch/conf.php
@@ -14,9 +14,9 @@ $WORKERCONF = [
     'commandsDelay' => 5,
     'commands' => [
         // token scopes needed: "bits:read channel:read:subscriptions moderation:read moderator:read:chatters"
-        'user' => 'api get users --unformatted --autopaginate -q login=spartalien',
+        'user' => 'api get /users --unformatted --autopaginate -q login=spartalien',
         'channel' => 'api get /channels --unformatted --autopaginate -q broadcaster_id=540195916',
-        'follower' => 'api get users follows --unformatted --autopaginate -q to_id=540195916',
+        'follower' => 'api get /users follows --unformatted --autopaginate -q to_id=540195916',
         'subscriber' => 'api get /subscriptions --unformatted --autopaginate -q broadcaster_id=540195916',
         'bitleader' => 'api get /bits/leaderboard --unformatted --autopaginate -q period=all -q count=100',
         'chatter' => 'api get /chat/chatters --unformatted --autopaginate -q broadcaster_id=540195916 -q moderator_id=540195916',
