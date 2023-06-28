952d56bdf9d06a5c5054720e7c148501f23c3d8e
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-06-28 14:19:55 +0200

    Separate init script

---
+++ src/lib/olay.ts
@@ -0,0 +1,14 @@
+const outputElement = document.querySelector('div.mod')
+const modHandle = new URL(document.location.href).searchParams.get('mod')
+
+
+if (
+    outputElement
+    && modHandle
+) {
+    const modScript = await import(`../mod/${modHandle}.js`)
+    new modScript.Mod(outputElement)
+}
+
+
+export {}
