8c51f83f826b698c54248c1f8fa4c6d5ef34805a
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-06-29 12:06:24 +0200

    ffff

--- app/lib/olay.js
+++
@@ -1,8 +0,0 @@
-const outputElement = document.querySelector('div.mod');
-const modHandle = new URL(document.location.href).searchParams.get('mod');
-if (outputElement
-    && modHandle) {
-    const modScript = await import(`../mod/${modHandle}.js`);
-    new modScript.Mod(outputElement);
-}
-export {};
