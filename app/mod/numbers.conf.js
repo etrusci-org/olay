cd27bca20055a0314b0decddb5be8a0c4efb8d73
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-07-06 14:57:00 +0200

    Numbers mod

---
+++ app/mod/numbers.conf.js
@@ -0,0 +1,37 @@
+export const modConf = {
+    /*
+    numbers update rate in milliseconds.
+    1000 milliseconds = 1 second.
+    */
+    updaterate: '1000',
+
+
+    /*
+    output type.
+    available types:
+    - random
+    - countup
+    - countdown
+    */
+    type: 'random',
+
+    /*
+    start and end range of the numbers.
+    if type is set to random, random numbers in this range will be picked.
+    if type is set to countup or countdown, numbers will go up or down in this range.
+    if type is set to countup or countdown, end can be set to 'none' to count forever.
+    */
+    start: '0',
+    end: '1000',
+
+    /*
+    whether to replace numbers with characters.
+    */
+    repnum: 'false',
+
+    /*
+    the character map to use for repnum.
+    1 - 5.
+    */
+    repmap: '1',
+}
