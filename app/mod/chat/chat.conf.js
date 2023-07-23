c719c5d921f304c3927ca3e85d058555b9ba3973
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-07-23 20:04:44 +0200

    Chat module defaults

--- app/mod/chat/chat.conf.js
+++ app/mod/chat/chat.conf.js
@@ -3,35 +3,8 @@
 export const modConf = {
     channels: `
         |spartalien
-        |doerthebecker
-        |helmahof
-        |sayaquita
-        |elleterese
-        |d_e_c_i_p_h_e_r
-        |djfolly_dnb
-        |flash_lab
-        |arianenova
-        |elleterese
-        |916junglist
-        |alexundbande
-        |chrisundermind
-        |couchfam
-        |crack1offski
-        |djlinny
-        |dunking_donuts
-        |flash_lab
-        |jck_official
-        |johnbbeta
-        |leodickinsonsumo
-        |orochihanma
-        |sneakylol
-        |tmocs
-        |vjpcat
     `,
-    ignore: `
-        |badboy
-        |badgirl
-    `,
+    ignore: ``,
     limit: '20',
     removeafter: 'never',
     addto: 'bottom',
