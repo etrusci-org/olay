6290ce6f0e1577de6719902643284efb9c6b4aa2
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-07-23 18:57:15 +0200

    New module: chat

---
+++ app/mod/chat/chat.conf.js
@@ -0,0 +1,45 @@
+// For help see chat.md
+
+export const modConf = {
+    channels: `
+        |spartalien
+        |doerthebecker
+        |helmahof
+        |sayaquita
+        |elleterese
+        |d_e_c_i_p_h_e_r
+        |djfolly_dnb
+        |flash_lab
+        |arianenova
+        |elleterese
+        |916junglist
+        |alexundbande
+        |chrisundermind
+        |couchfam
+        |crack1offski
+        |djlinny
+        |dunking_donuts
+        |flash_lab
+        |jck_official
+        |johnbbeta
+        |leodickinsonsumo
+        |orochihanma
+        |sneakylol
+        |tmocs
+        |vjpcat
+    `,
+    ignore: `
+        |badboy
+        |badgirl
+    `,
+    limit: '20',
+    removeafter: 'never',
+    addto: 'bottom',
+    timestampformat: '{hour}:{minute}:{second}',
+    usercolor: 'true',
+    emotetheme: 'light',
+    autoscroll: 'true',
+    repnum: 'false',
+    repmap: '1',
+    joininterval: '3000',
+}
