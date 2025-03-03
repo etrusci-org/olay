58d9cce777a5de283f109752134c06905498f98a
arT2 <60965610+etrusci-org@users.noreply.github.com>
2025-03-03 15:47:51 +0100

    make elcolorfader lib a module

--- fetchdep.sh
+++ fetchdep.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-curl -o ./src/lib/elcolorfader.ts https://raw.githubusercontent.com/etrusci-org/elcolorfader/refs/heads/main/src/elcolorfader.ts
+curl -o ./src/lib/elcolorfader.mts https://raw.githubusercontent.com/etrusci-org/elcolorfader/refs/heads/main/src/elcolorfader.ts
+sed -i 's/class ElColorfader/export class ElColorfader/g' ./src/lib/elcolorfader.mts
+
 curl -o ./olay/lib/vendor/tmi.min.js https://raw.githubusercontent.com/tmijs/tmi.js/refs/heads/main/dist/tmi.min.js
