d4d3c7c26aca46e8aa87fab1b06f5961b93d9877
arT2 <60965610+etrusci-org@users.noreply.github.com>
2025-03-20 15:41:07 +0100

    fixed tmi.js version

--- fetchdep.sh
+++ fetchdep.sh
@@ -8,6 +8,6 @@ sed -i 's/class ElColorfader/export class ElColorfader/g' ./src/lib/elcolorfader
 curl -o ./src/lib/elfloater.mts https://raw.githubusercontent.com/etrusci-org/elfloater/refs/heads/main/src/elfloater.ts
 sed -i 's/class ElFloater/export class ElFloater/g' ./src/lib/elfloater.mts
 
-# tmi.js <https://github.com/tmijs/tmi.js>
+# tmi.js 1.8.5 <https://github.com/tmijs/tmi.js>
 mkdir -p ./olay/lib/vendor/
-curl -o ./olay/lib/vendor/tmi.min.js https://raw.githubusercontent.com/tmijs/tmi.js/refs/heads/main/dist/tmi.min.js
+curl -o ./olay/lib/vendor/tmi.min.js https://raw.githubusercontent.com/tmijs/tmi.js/7691acb4b4b1fbebabd0a85f26b6444e7d6c573e/dist/tmi.min.js
